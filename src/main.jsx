a752ae1be8ba46b0ec5b3fae230e2d15d1dda4d8
eric406-yeboah <ericusyeboah406@gmail.com>
2024-02-03 16:21:10 +0000

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+// index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+import App from './assets/page';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+
+
+// import App from './assets/page';
+
+// ReactDOM.createRoot (document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+  
+// )
+
